adbedbb5bd4a8ddd5d84f23ae0587c6a25b3ea75
TypKing <ivannikov.cawa2@mail.ru>
2021-05-14 17:05:34 +0300

    Добавьте файлы проекта.

---
+++ Sem_13-14/Sem_13-14.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+    string strData;
+    ifstream inp("input.txt");
+    int n;
+    cout << "Какую строку вы хотите получить из файла?" << endl;
+    cin >> n;
+    inp.seekg(0, ios::beg);
+    getline(inp, strData);
+    int len = strData.length();
+    cout << "Ваша строка: " << endl;
+    inp.seekg((n - 1) * (len + 2), ios::beg);
+    getline(inp, strData);
+    cout << strData;
+}
+
